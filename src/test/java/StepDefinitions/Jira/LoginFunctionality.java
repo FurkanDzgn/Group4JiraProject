24fa81af0d8e9b4974ebefa69613678e6563d6aa
furkandsgn <furkanduzgunpa@gmail.com>
2020-07-20 14:49:26 -0500

    login Functionality

---
+++ src/test/java/StepDefinitions/Jira/LoginFunctionality.java
@@ -0,0 +1,21 @@
+package StepDefinitions.Jira;
+
+import Jira.Localhost8080.LoginPage;
+import Utils.ConfigReader;
+import Utils.Driver;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+
+public class LoginFunctionality {
+
+    WebDriver driver = Driver.getDriver();
+    LoginPage loginPage = new LoginPage();
+
+    @Given("The user enter username and password")
+    public void the_user_enter_username_and_password() {
+        driver.get(ConfigReader.getProperty("urlJira"));
+        loginPage.username.sendKeys(ConfigReader.getProperty("userName"));
+        loginPage.password.sendKeys(ConfigReader.getProperty("password"));
+        loginPage.submitButton.click();
+    }
+}
